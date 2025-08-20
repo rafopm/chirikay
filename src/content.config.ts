bed438f6675cf72ba3e4181e626aa51bc613ac9a
rafopm <rafopm@gmail.com>
2025-08-20 00:34:49 +0000

    update error horizontaltimeLine

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,20 @@
-import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		order: z.number(),
-		title: z.string(),
-		metatitle: z.string(),
-		description: z.string(),
-		metadescription: z.string(),
-		imgurl: z.string(),
-		imgthumburl: z.string(),
-		alt: z.string(),
-		pubDate: z.coerce.date(),
-	}),
+  // Tipo de colección (markdown o data)
+  type: 'content',
+  // Schema para frontmatter
+  schema: z.object({
+    order: z.number(),
+    title: z.string(),
+    metatitle: z.string(),
+    description: z.string(),
+    metadescription: z.string(),
+    imgurl: z.string(),
+    imgthumburl: z.string(),
+    alt: z.string(),
+    pubDate: z.coerce.date(),
+  }),
 });
 
-
-
-
-export const collections = { blog  };
+export const collections = { blog };
